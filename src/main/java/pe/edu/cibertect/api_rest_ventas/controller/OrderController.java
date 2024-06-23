4531dfa6712b07448c4eeb93be4f0673c7806ad9
gustavoqooo <luisangel.salvat@gmail.com>
2024-06-23 09:51:01 -0500

    controller del recurso Order

--- src/main/java/pe/edu/cibertect/api_rest_ventas/controller/OrderController.java
+++ src/main/java/pe/edu/cibertect/api_rest_ventas/controller/OrderController.java
@@ -1,4 +1,29 @@
 package pe.edu.cibertect.api_rest_ventas.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.cibertect.api_rest_ventas.model.bd.Order;
+import pe.edu.cibertect.api_rest_ventas.service.IOrderService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1/order")
 public class OrderController {
+    private IOrderService iOrderService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Order>> listarOrdenes(){
+        List<Order> orderList = new ArrayList<>(iOrderService.listarOrdenes());
+        if(orderList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(orderList, HttpStatus.OK);
+    }
 }
