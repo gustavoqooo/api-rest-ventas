3cf2960d3a1a7a3f128715afa1648ac8647e568c
gustavoqooo <luisangel.salvat@gmail.com>
2024-06-30 11:38:06 -0500

    Métodos del Filtro JWT

---
+++ src/main/java/pe/edu/cibertect/api_rest_ventas/security/FiltroJWTAutorizacion.java
@@ -0,0 +1,55 @@
+package pe.edu.cibertect.api_rest_ventas.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FiltroJWTAutorizacion extends OncePerRequestFilter {
+
+    private final String CLAVE = "@Cibertec2023";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+    }
+
+    private void cargarAutorizaciones(Claims claims){
+        List<String> autorizaciones = (List<String>)claims.get("authorities");
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        claims.getSubject(),
+                        null,
+                        autorizaciones.stream().map(SimpleGrantedAuthority::new)
+                                .collect(Collectors.toList())
+                );
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+
+    private Claims validarToken(HttpServletRequest request){
+        String token = request.getHeader("Authorization")
+                .replace("Bearer ", "");
+        return Jwts.parser().setSigningKey(CLAVE.getBytes())
+                .parseClaimsJws(token).getBody();
+    }
+
+    private boolean validarUsoToken(HttpServletRequest request){
+        String autenticacion = request.getHeader("Authorization");
+        if(autenticacion == null  || !autenticacion.startsWith("Bearer ")){
+            return false;
+        }
+        return true;
+    }
+
+}
