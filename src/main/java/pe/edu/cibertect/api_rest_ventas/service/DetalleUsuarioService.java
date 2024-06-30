d046e19194182bfc44979b54c6792398192f89b6
gustavoqooo <luisangel.salvat@gmail.com>
2024-06-30 11:04:45 -0500

    Detalle de usuario terminado

--- src/main/java/pe/edu/cibertect/api_rest_ventas/service/DetalleUsuarioService.java
+++ src/main/java/pe/edu/cibertect/api_rest_ventas/service/DetalleUsuarioService.java
@@ -1,13 +1,56 @@
 package pe.edu.cibertect.api_rest_ventas.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertect.api_rest_ventas.model.bd.Rol;
+import pe.edu.cibertect.api_rest_ventas.model.bd.Usuario;
+import pe.edu.cibertect.api_rest_ventas.repository.UsuarioRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+@Service
 public class DetalleUsuarioService implements UserDetailsService {
+    private UsuarioRepository usuarioRepository;
     @Override
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
-        return null;
+        Usuario usuario =obtenerUsuarioXNomusuario(username);
+        return autenticacionUsuario(
+                usuario,
+                obtenerListaRoles(usuario.getRoles())
+        );
     }
+    public Usuario obtenerUsuarioXNomusuario(String nomusuario){
+        return usuarioRepository.findByNomusuario(nomusuario);
+    }
+    public List<GrantedAuthority> obtenerListaRoles(Set<Rol> roles){
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        for(Rol rol : roles){
+            authorityList.add(new
+                    SimpleGrantedAuthority("ROLE_"+rol.getNomrol()));
+        }
+        return  authorityList;
+    }
+
+    private UserDetails autenticacionUsuario(
+            Usuario usuario, List<GrantedAuthority> authorityList
+    ){
+        return new User(
+                usuario.getNomusuario(),
+                usuario.getPassword(),
+                usuario.getActivo(),
+                true, true, true,
+                authorityList
+        );
+    }
+
 }
