8f4814df89aa7193b9dec963f76a066984177b1a
gustavoqooo <luisangel.salvat@gmail.com>
2024-06-30 09:27:14 -0500

    habilitando cors origin de manera global

--- src/main/java/pe/edu/cibertect/api_rest_ventas/ApiRestVentasApplication.java
+++ src/main/java/pe/edu/cibertect/api_rest_ventas/ApiRestVentasApplication.java
@@ -2,6 +2,9 @@ package pe.edu.cibertect.api_rest_ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiRestVentasApplication {
@@ -9,5 +12,18 @@ public class ApiRestVentasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestVentasApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer configGlobalCors(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("api/v1/**")
+						.allowedMethods("GET", "POST", "PUT")
+						.allowedOrigins("*");
+			}
+		};
+	}
+
 
 }
